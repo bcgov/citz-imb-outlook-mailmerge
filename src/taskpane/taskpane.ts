0db30cbd09526b3e67b47d41c1fb70b03d3453be
Scott Toews <scott.toews@gov.bc.ca>
2025-01-07 11:29:39 -0800

    set up html for the pane

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -10,5 +10,19 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("sideload-msg")!.style.display = "none";
     document.getElementById("app-body")!.classList.add("ms-welcome__main");
+    document.getElementById("attachments")?.addEventListener("change", handleAttachments, false);
   }
 });
+
+async function handleAttachments(event: Event) {
+  const attachments = (event.target as HTMLInputElement).files;
+  if (!attachments || attachments.length === 0) {
+    return;
+  }
+
+  const attachmentElement = document.getElementById("attachmentsList");
+
+  Array.from(attachments).map((file) => {
+    attachmentElement!.innerHTML += `<li class="listItem ms-ListItem-tertiaryText">${file.name}</li>`;
+  });
+}
