0731aa64a0b6c8fb898fefe372ff14ddc709e6c5
Scott Toews <scott.toews@gov.bc.ca>
2025-01-14 08:09:52 -0800

    add app id info

--- src/taskpane/mailMerge.ts
+++ src/taskpane/mailMerge.ts
@@ -1,3 +1,5 @@
+/* global Office */
+
 import { extractContacts, extractTemplate } from "./helper";
 
 export async function runMailMerge(contactsFile: File, templateFile: File, attachmentFiles?: FileList) {
@@ -6,4 +8,20 @@ export async function runMailMerge(contactsFile: File, templateFile: File, attac
   console.log("contacts", contacts);
   const template = await extractTemplate(templateFile);
   console.log("template", template);
+  console.log("office", Office.context.mailbox.item);
+
+  //sendEmail(templateFile);
+}
+
+async function sendEmail(templateFile: File) {
+  const template = await extractTemplate(templateFile);
+  console.log("template", template);
+  console.log("office", Office.context.mailbox.item);
+
+  Office.context.mailbox.displayNewMessageForm({
+    toRecipients: ["scott.toews@gov.bc.ca"],
+    subject: "Test Mail Merge",
+    body: template, // Use the extracted template as the body
+    // attachments: attachmentFiles ? Array.from(attachmentFiles) : []
+  });
 }
