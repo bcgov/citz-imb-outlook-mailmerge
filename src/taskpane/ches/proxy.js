8f3b4bdeed0173b17dd39fcc5c10439f5c392903
Scott Toews <scott.toews@gov.bc.ca>
2025-01-15 11:17:29 -0800

    add proxy server

---
+++ src/taskpane/ches/proxy.js
@@ -0,0 +1,56 @@
+import express, { json } from "express";
+import fetch from "node-fetch";
+
+/* global process */
+
+const app = express();
+const port = 3001;
+
+app.use(json());
+
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.post("/send-email", async (req, res) => {
+  try {
+    const tokenResponse = await fetch(
+      "https://dev.loginproxy.gov.bc.ca/auth/realms/comsvcauth/protocol/openid-connect/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          grant_type: "client_credentials",
+        }),
+      }
+    );
+
+    const { access_token } = await tokenResponse.json();
+
+    const response = await fetch("https://ches-dev.api.gov.bc.ca/api/v1/email", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req.body),
+    });
+
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Proxy server running at http://localhost:${port}`);
+});
